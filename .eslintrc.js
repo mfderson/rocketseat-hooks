12a37faf6965258236b1618e887647ed71885dea
Anderson Freitas <mfderson@gmail.com>
2020-01-28 18:52:45 -0300

    Configurando prettier, eslint e editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    es6: true
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "linebreak-style": ["error", "windows"],
+    "linebreak-style": 0,
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [
+      "warn",
+      {
+        extensions: [".jsx", ".js"]
+      }
+    ],
+    "import/prefer-default-export": "off",
+    "no-param-reassign": "off",
+    "no-console": ["error", { allow: ["tron"] }]
+  }
+};
