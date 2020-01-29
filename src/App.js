b5a2f83bf6faef9782d9117d07bcaf26c3034799
Anderson Freitas <mfderson@gmail.com>
2020-01-28 21:11:29 -0300

    Hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // O primeiro parâmetro é o nome da variável que armazenará o estada
+  // O segundo é o nome da função que altera o estado
   const [tech, setTech] = useState(['ReactJS', 'ReactNative']);
   const [newTech, setNewTech] = useState('');
 
@@ -8,6 +10,29 @@ function App() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  // A função será executada apenas uma vez, dado que o segundo parâmetro
+  // é um array vazio.
+  // -----------------------------
+  // Simula o didMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // 1. O primeiro parâmetro do método useEffect é a função que vai ser
+  // executada;
+  // 2. O segundo parâmetro é quando ela vai ser executada. Esse é um array
+  // de dependências que fica monitorando alterações em certas variáveis.
+  // Se for passado um array vazio, o método executará apenas uma vez.
+  // ----------------------
+  // Simula o didUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
